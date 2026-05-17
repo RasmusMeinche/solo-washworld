792584a7242fc3a4af11cf8947271d23b2ca2ceb
RasmusMeinche <rame0001@stud.kea.dk>
2026-05-17 19:19:49 +0200

    Sign-up + Verify account with key sent from email works in backend

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 import uuid
 import time
 
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+
 import x
 
 import smtplib
@@ -25,19 +28,62 @@ def sign_up():
         user_pk = uuid.uuid4().hex
         user_first_name = x.validate_user_first_name()
         user_last_name = x.validate_user_last_name()
-        user_email = x.validate_email( request.form.get("user_email", "" ))
+        user_email = x.validate_user_email( request.form.get("user_email", "" ))
         user_password = x.validate_user_password()
-        user_hashed_password = generate_password_hash(user_password)
-        verification_key = uuid.uuid4().hex
-
-
-
+        user_password_hashed = generate_password_hash(user_password)
+        user_created_at = time.time()
+        ic(user_created_at)
+        user_verification_key = uuid.uuid4().hex
+        user_verified_at = 0
+        user_reset_password_key = uuid.uuid4().hex + uuid.uuid4().hex
 
         db, cursor = x.db()
+        q = "INSERT INTO users (user_pk, user_first_name, user_last_name, user_email, user_password_hashed, user_created_at, user_verification_key, user_verified_at, user_reset_password_key) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(q, (user_pk, user_first_name, user_last_name, user_email, user_password_hashed, user_created_at, user_verification_key, user_verified_at, user_reset_password_key))
+        db.commit()
 
-        return "We have sent a confirmation email to your account"
+        activation_email = render_template("email_welcome.html", user_verification_key=user_verification_key)
+
+        x.send_email("Activate your account", activation_email)
+
+        return "We have sent a confirmation email to your account", 201
     except Exception as ex:
+        ic(ex)
+        if "company_exception user_first_name" in str(ex):
+            return "First name must be between {x.USER_FIRST_NAME_MIN} and {x.USER_FIRST_NAME_MAX} characters", 400
+            
+        if "company_exception last_name" in str(ex):
+            return "Last name must be between {x.USER_LAST_NAME_MIN} and {x.USER_LAST_NAME_MAX} characters", 400
 
+        return str(ex), 500
     finally:
         if "cursor" in locals(): cursor.close()
         if "db" in locals(): db.close()
+
+##############################
+@app.get("/verify/<key>")
+def verify_account(key):
+    try:
+        key = x.validate_uuid4(key)
+        db, cursor = x.db()
+        user_verified_at = int(time.time())
+        q = """
+            UPDATE users
+            SET user_verified_at = %s
+            WHERE user_verification_key = %s AND user_verified_at = 0
+        """
+        cursor.execute(q, (user_verified_at, key))
+        db.commit()
+        if cursor.rowcount == 0:
+            return "user already verified"
+
+        return f"Welcome to the system, you are verified"
+    except Exception as ex: 
+        ic(ex)
+        if "company_exception uuid4 invalid" in str(ex):
+            return "Invalid key", 400
+
+        return str(ex), 500
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()  
--- backend/x.py
+++ backend/x.py
@@ -2,6 +2,12 @@ from flask import request, make_response
 import mysql.connector
 import re # Regular expressions also called Regex
 from functools import wraps
+from icecream import ic
+ic.configureOutput(prefix=f"_____ | ", includeContext=True)
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 ##############################
 def db():
@@ -45,14 +51,14 @@ USER_LAST_NAME_MAX = 20
 REGEX_USER_LAST_NAME = f"^.{{{USER_LAST_NAME_MIN},{USER_LAST_NAME_MAX}}}$"
 def validate_user_last_name():
     user_last_name = request.form.get("user_last_name")
-    if not re.match(REGEX_USER_LAST_NAME, user_first_name):
-        raise Exception("company_exception user_first_name")
+    if not re.match(REGEX_USER_LAST_NAME, user_last_name):
+        raise Exception("company_exception user_last_name")
     return user_last_name
 
 ##############################
 REGEX_EMAIL = "^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
-def validate_user_email( email ):
-    user_email = email.strip()
+def validate_user_email( user_email ):
+    validate_user_email = user_email.strip()
     if not re.match(REGEX_EMAIL, user_email):
         raise Exception("company_exception user_email")
     return user_email
@@ -62,7 +68,64 @@ USER_PASSWORD_MIN = 8
 USER_PASSWORD_MAX = 50
 REGEX_USER_PASSWORD = f"^.{{{USER_PASSWORD_MIN},{USER_PASSWORD_MAX}}}$"
 def validate_user_password():
-    user_password = request.form.get("user_password")
+    user_password = request.form.get("user_password", "").strip()
     if not re.match(REGEX_USER_PASSWORD, user_password):
         raise Exception("company_exception user_password")
     return user_password
+
+##############################
+# 0 to 9 letters a to f
+REGEX_UUID4 = "^[0-9a-f]{32}$"
+def validate_uuid4(uuid4):
+    uuid = uuid4.strip()
+    if not re.match(REGEX_UUID4, uuid):
+        raise Exception("company_exception uuid4 invalid")
+    return uuid
+
+
+##############################
+REGEX_UUID4_PARANOIA = "^[0-9a-f]{64}$"
+def validate_uuid4_paranoia(uuid4):
+    uuid = uuid4.strip()
+    if not re.match(REGEX_UUID4, uuid):
+        raise Exception("company_exception paranoia")
+    return uuid
+
+##############################
+def send_email(subject, activation_email):
+    try:    
+        # Create a gmail
+        # Enable (turn on) 2 step verification/factor in the google account manager
+        # Visit: https://myaccount.google.com/apppasswords
+        # Copy the key : 
+
+        # Email and password of the sender's Gmail account
+        sender_email = "rasmus.meinche@gmail.com"
+        password = "dqid pudd nhdb wbmb"  # If 2FA is on, use an App Password instead
+
+        # Receiver email address
+        receiver_email = "rasmus.meinche@gmail.com"
+        
+        # Create the email message
+        message = MIMEMultipart()
+        message["From"] = "Wash World"
+        message["To"] = receiver_email
+        message["Subject"] = subject
+
+        # Body of the email
+        # body = f"""<h1>Hi</h1><h2>Hi again</h2>"""
+        message.attach(MIMEText(activation_email, "html"))
+
+        # Connect to Gmail's SMTP server and send the email
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()  # Upgrade the connection to secure
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+        ic("Email sent successfully!")
+
+        return "email sent"
+       
+    except Exception as ex:
+        return "cannot send email", 500
+    finally:
+        pass
